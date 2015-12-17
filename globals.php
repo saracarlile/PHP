90f48514ddfdc714ee3e967beec351456102294e
Sara Carlile <sara.elizabeth.wood@gmail.com>
2015-12-17 15:06:57 -0600

    changes to global

--- globals.php
+++ globals.php
@@ -2,7 +2,7 @@
 /* $GLOBALS in php is an associative array that contains references to all variables which are currently defined in the global
 scope of the script */
 
-	function WritePage($pagetitle, $description, $pageheading, $forwardlink = "", $backlink = "") {
+	function WritePage($pagetitle, $description, $pageheading, $forwardlink = "", $backlink = "") {  /*not all pages need forward and back links, therefore a default value of blank is assigned so the function can be called without these two arguements */
 		$GLOBALS["forwardlink"] = "$forwardlink"; /* assigns the global variable forwardlink to the arguement passed into $forwardlink in the function WritePage */
 		$GLOBALS["backlink"] = "$backlink";
 		echo "<br>"."<br>".$pagetitle."<br>".$description."<br>".$pageheading."<br>".$GLOBALS["forwardlink"]."<br>".$GLOBALS["backlink"];	
@@ -13,17 +13,23 @@ WritePage(
 		"Page Title - PHP Globals",
 		"A page description for PHP Globals goes here.",
 		"PHP Globals - Example",
-		"optional-forwardlinks",
-		"optional-backlink" 
+		"/link/somplace/forward/",
+		"/link/someplace/back/" 
 
 	);
-	
 
+
+function PrevNext($back	, $forward){
+	echo "<a href=".$back.">Back</a>";
+	echo "<a href=".$forward.">Forward</a>";
+	
+}
+	
 ?>
 
 
 
 
-<div id="centerWrap">
+<div id="center">
 	<?php PrevNext($GLOBALS["backlink"]	, $GLOBALS["forwardlink"]); ?>
-	<div id="center">
+</div>
